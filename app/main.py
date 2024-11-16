5efc81b1caf71c63038cb5d6e25da7273d8fe92e
林贵南 <guinan.lin@foxmail.com>
2024-11-17 00:38:12 +0800

    update

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # 添加 CORS 中间件
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://192.168.8.135:3000"],  # 允许的源
+    allow_origins=["http://192.168.8.135:3000","https://read-notion-help-docs.onrender.com"],  # 允许的源
     allow_credentials=True,
     allow_methods=["*"],  # 允许所有方法
     allow_headers=["*"],  # 允许所有头部
